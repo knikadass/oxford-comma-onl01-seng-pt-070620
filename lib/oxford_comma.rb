6157ddb896881f75250f0e55e3a13915ccd5d8fe
Knika Dass <knikadass@gmail.com>
2020-07-14 21:33:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
  elsif array.length == 2 
    array.join(" and ")
  elsif array.length == 3
-   array[-1] = (" and #{array[-1]}").join
+   array[-1] = " and #{array[-1]}"
+   join
    
  end
 end
