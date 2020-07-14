654ab9f1b78a22f1be055a47d8ecbde757c52435
Knika Dass <knikadass@gmail.com>
2020-07-14 20:53:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,6 @@
+require "pry"
 def oxford_comma(array)
-  if array.length == 1 
-    array.join(",")
-  elsif array.length == 2 
-   array[-1].insert(0, "and")
-   array.join(" ")
-  elsif array.length >=3
-   array[-1].insert(0, "and")
-   array.join(", ")
-  else
-    array.join
-  end
+  binding.pry
 end
 
 end
